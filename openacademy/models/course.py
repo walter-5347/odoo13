ad4e33384a70c57f17ac7ba27d68f1e0325f8746
walter-5347 <47955493+walter-5347@users.noreply.github.com>
2020-07-15 01:57:10 -0600

    [ADD]Relational fields

--- openacademy/models/course.py
+++ openacademy/models/course.py
@@ -17,4 +17,7 @@ class Session(models.Model):
     duration = fields.Float(digits=(6, 2), help="Duration in days")
     seats = fields.Integer(string="Number of seats")
 
-    
+    instructor_id = fields.Many2one('res.partner', string="Instructor")
+    course_id = fields.Many2one('openacademy.course',
+    ondelete='cascade', string="Course", required=True)
+
