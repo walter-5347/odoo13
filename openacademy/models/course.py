70be78d17fd4294ad746f5590696da07f812b85f
walter-5347 <47955493+walter-5347@users.noreply.github.com>
2020-07-15 02:05:54 -0600

    [errore]correccion

--- openacademy/models/course.py
+++ openacademy/models/course.py
@@ -8,6 +8,9 @@ class Course(models.Model):
     name = fields.Char(string="Title", required=True)
     description = fields.Text()
 
+     responsible_id = fields.Many2one('res.users',
+    ondelete='set null', string="Responsible", index=True)
+
 class Session(models.Model):
     _name = 'openacademy.session'
     _description = "OpenAcademy Sessions"
