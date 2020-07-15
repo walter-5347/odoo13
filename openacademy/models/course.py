4e20842207893c3b61bc5018d994f0eca8c8b7ae
walter-5347 <47955493+walter-5347@users.noreply.github.com>
2020-07-15 02:09:08 -0600

    [errores]

--- openacademy/models/course.py
+++ openacademy/models/course.py
@@ -8,7 +8,7 @@ class Course(models.Model):
     name = fields.Char(string="Title", required=True)
     description = fields.Text()
 
-     responsible_id = fields.Many2one('res.users',
+    responsible_id = fields.Many2one('res.users',
     ondelete='set null', string="Responsible", index=True)
 
 class Session(models.Model):
